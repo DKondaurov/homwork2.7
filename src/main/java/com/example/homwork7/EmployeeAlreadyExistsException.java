eafa570988f4ab2e17609510e21e90c9a4f7672e
homework <9874@bk.ru>
2022-02-01 00:42:43 +0300

    Done

--- src/main/java/com/example/homwork7/EmployeeAlreadyExistsException.java
+++ src/main/java/com/example/homwork7/EmployeeAlreadyExistsException.java
@@ -3,7 +3,7 @@ package com.example.homwork7;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CREATED)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class EmployeeAlreadyExistsException extends RuntimeException {
 
 }
